9345bd3c6d6d02d3efb00fed7ce6c6e51772c4b2
Dinh Quang Tan <dinhquangtan@outlook.com>
2022-01-18 23:24:38 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_concepts/logic/cubit/counter_cubit.dart';
+import 'package:flutter_bloc_concepts/presentation/router/app_router.dart';
+import 'package:flutter_bloc_concepts/presentation/screen/home_screen.dart';
+import 'package:flutter_bloc_concepts/presentation/screen/second_screen.dart';
+import 'package:flutter_bloc_concepts/presentation/screen/third_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AppRouter _appRouter = AppRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      onGenerateRoute: _appRouter.onGenerateRoute,
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _appRouter.dispose();
+  }
+}
